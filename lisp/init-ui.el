eaef351e7b2301af08fad57782d69fbd681a2c76
onlyedit <845011397@qq.com>
2024-05-07 16:47:20 +0800

    add font-install function

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -207,6 +207,10 @@
 (use-package minions
   :hook (doom-modeline-mode . minions-mode))
 
+;; Font
+(defun font-installed-p (font-name)
+  "Check if font with FONT-NAME is available."
+  (find-font (font-spec :name font-name)))
 ;; Icons
 (use-package nerd-icons
   :config
